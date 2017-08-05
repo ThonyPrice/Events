8e79bc083b686b9a4fa0d054a335cc2a0394bf68
Thony Price <thonyprice@gmail.com>
2017-08-05 12:10:43 +0200

    Added background image

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -5,9 +5,48 @@ import AwesomeComponent from './AwesomeComponent.jsx';
 class App extends React.Component {
   render () {
     return (
-      <div>
-        <p> Hello React Hello!</p>
-        <AwesomeComponent />
+      <div
+      style={
+        {margin: '0'},
+        {padding: '0'}
+      }>
+        <div className="container header-section"
+        style={
+          {margin: '0'},
+          {padding: '0'},
+          {position: 'relative'}
+        }>
+          <div className="background-image"
+          style={
+            {margin: '0'},
+            {padding: '0'},
+            {position: 'relative'}
+          }>
+            <img
+              src="app/img/gb-background.png"
+              className="img-responsive"
+              id="top-bg-picture"
+              alt="Background"
+            ></img>
+
+            <div className="col-3 logo"
+            style={
+              {background: 'green'},
+              {padding: '0'},
+              {position: 'absolute'}
+            }>
+              <img
+                src="app/img/globuzzer-big-logo.png"
+                className="img-responsive text-left"
+                id="top-bg-logo"
+                alt="Logo"
+              ></img>
+            </div>
+
+          </div>
+
+          <AwesomeComponent />
+        </div>
       </div>
     );
   }
