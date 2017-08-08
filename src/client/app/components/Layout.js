f87212c973ce4b365723cea192150e3a18cf6bbf
Thony Price <thonyprice@gmail.com>
2017-08-08 11:13:28 +0200

    Corrected responsive behaviour of navbar

--- src/client/app/components/Layout.js
+++ src/client/app/components/Layout.js
@@ -6,18 +6,71 @@ import Content from "./Content";
 export default class Layout extends React.Component {
   render() {
     return (
-      <div className="group-bg"
-      style={{
-        position: 'relative'
-      }}>
-        <BackgroundImage />
-        <div style={{
-          position: 'absolute',
-          top: '0',
-          width: '100%'
-        }}>
-          <Navbar />
+      <div>
+        <div>
+          <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <a className="navbar-brand" href="#">GLOBUZZER</a>
+
+            <div className="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo02">
+              <ul className="navbar-nav">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">HOME<span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">ABOUT</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">SIGN IN</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">EVENTS</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
         </div>
+
+        <section className="first">
+
+          <div className="container">
+
+
+          </div>
+
+        </section>
+
+        <section id="services">
+
+          <div className="container">
+
+            <h1>Services here is more content</h1>
+
+          </div>
+
+        </section>
+
+        <section>
+
+          <div className="container">
+
+            <h1>Here is even more content</h1>
+
+          </div>
+
+        </section>
+
+        <section>
+
+          <div className="container">
+
+            <h1>Here is random more content</h1>
+
+          </div>
+
+        </section>
       </div>
     );
   }
